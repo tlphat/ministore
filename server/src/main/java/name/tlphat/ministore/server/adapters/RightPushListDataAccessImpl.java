8cb8456cd95dac3a03d4d4327683c05056b1f8f9
Phat Tang <tlphat18@apcs.vn>
2024-02-10 20:44:31 +0700

    feat(server): implement data access layer

---
+++ server/src/main/java/name/tlphat/ministore/server/adapters/RightPushListDataAccessImpl.java
@@ -0,0 +1,21 @@
+package name.tlphat.ministore.server.adapters;
+
+import name.tlphat.ministore.server.store.DataStore;
+import name.tlphat.ministore.server.usecases.exceptions.DataAccessException;
+import name.tlphat.ministore.server.usecases.ports.RightPushListDataAccess;
+
+public class RightPushListDataAccessImpl implements RightPushListDataAccess {
+
+    private final DataStore dataStore;
+
+    public RightPushListDataAccessImpl(DataStore dataStore) {
+        this.dataStore = dataStore;
+    }
+
+    @Override
+    public void appendRightToList(String key, String value) {
+        if (!dataStore.appendToList(key, value)) {
+            throw new DataAccessException();
+        }
+    }
+}
